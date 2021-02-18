8987483aa1228bac72b54c1a78fc7bf698fc3960
JD Marty <joshua.dmarty@gmail.com>
2021-02-17 21:29:49 -0800

    added folders for components and utilities

---
+++ client/src/utils/API.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+const API = {
+    // create user from username, email, and password
+    createUser: function(userData) {
+        return axios.post("/api/user/create", userData)
+    },
+
+    // login user with email and password
+    loginUser: function(userData) {
+        return axios.post("/api/user/login", userData)
+    },
+
+    // logout user
+    logoutUser: function() {
+        return axios.post("/api/user/logout")
+    }
+}
