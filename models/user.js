d74b823a3068e3680cd13095cf038f93bc0e816f
JD Marty <joshua.dmarty@gmail.com>
2021-02-17 19:24:54 -0800

    added user model with bcrpyt

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    trim: true,
+    required: "Username is required",
+  },
+
+  password: {
+    type: String,
+    trim: true,
+    required: "Password is Required",
+    validate: [({ length }) => length >= 8, "Password should be longer than 8 characters."],
+  },
+
+  email: {
+    type: String,
+    trim: true,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+  },
+
+  userCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Method to hash password
+UserSchema.methods.hashPassword = async () => {
+    this.password = await bcrypt(this.password, 10);
+    return this.password
+}
+
+//Method to compare passwords
+UserSchema.methods.comparePassword = (loginPw) => {
+    return bcrypt.compareSync(loginPw, this.password)
+}
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User
