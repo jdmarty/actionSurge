17bcbc7a8079984565537574adef43508345c13d
JD Marty <joshua.dmarty@gmail.com>
2021-02-19 20:23:59 -0800

    added links to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,57 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div className="py-9 md:px-9 px-4 flex justify-center">
-      <div className="md:grid grid-cols-3 gap-6 w-full">
-        {/* Animation layer */}
-        <div className="transition duration-250 ease-in-out transform hover:scale-110">
-          {/* Design Layer */}
-          <div className="mt-2 border bg-gray-900 rounded-md">
-            {/* Decoration */}
-            <div className="border flex justify-center items-center py-24">
-              <i class="fas fa-helmet-battle text-white text-9xl"></i>
-            </div>
-            {/* Description */}
-            <div className="border py-8">
-              <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
-                Create Player
-              </h1>
-            </div>
-          </div>
-        </div>
-        {/* Animation layer */}
-        <div className="transition duration-250 ease-in-out transform hover:scale-110">
-          {/* Design Layer */}
-          <div className="mt-2 border bg-gray-900 rounded-md">
-            {/* Decoration */}
-            <div className="border flex justify-center items-center py-24">
-              <i class="fas fa-user-edit text-white text-9xl"></i>
-            </div>
-            {/* Description */}
-            <div className="border py-8">
-              <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
-                Edit Player
-              </h1>
+      {/* Grid Wrapper */}
+      <div className="md:grid grid-cols-3 gap-10 w-full">
+        <Link to="/create-player">
+          {/* Animation layer */}
+          <div className="transition duration-250 ease-in-out transform hover:scale-110">
+            {/* Design Layer */}
+            <div className="mt-2 border bg-gray-900 rounded-md">
+              {/* Decoration */}
+              <div className="border flex justify-center items-center py-24">
+                <i class="fas fa-helmet-battle text-white text-9xl"></i>
+              </div>
+              {/* Description */}
+              <div className="border py-8">
+                <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
+                  Create Player
+                </h1>
+              </div>
             </div>
           </div>
-        </div>
-        {/* Animation layer */}
-        <div className="transition duration-250 ease-in-out transform hover:scale-110">
-          {/* Design Layer */}
-          <div className="mt-2 border bg-gray-900 rounded-md">
-            {/* Decoration */}
-            <div className="border flex justify-center items-center py-24">
-              <i class="fas fa-swords text-white text-9xl"></i>
-            </div>
-            {/* Description */}
-            <div className="border py-8">
-              <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
-                Run Battle
-              </h1>
+        </Link>
+
+        <Link to="/edit-player">
+          {/* Animation layer */}
+          <div className="transition duration-250 ease-in-out transform hover:scale-110">
+            {/* Design Layer */}
+            <div className="mt-2 border bg-gray-900 rounded-md">
+              {/* Decoration */}
+              <div className="border flex justify-center items-center py-24">
+                <i class="fas fa-user-edit text-white text-9xl"></i>
+              </div>
+              {/* Description */}
+              <div className="border py-8">
+                <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
+                  Edit Player
+                </h1>
+              </div>
             </div>
           </div>
-        </div>
+        </Link>
+
+        <Link to="/battle">
+          {/* Animation layer */}
+          <div className="transition duration-250 ease-in-out transform hover:scale-110">
+            {/* Design Layer */}
+            <div className="mt-2 border bg-gray-900 rounded-md">
+              {/* Decoration */}
+              <div className="border flex justify-center items-center py-24">
+                <i class="fas fa-swords text-white text-9xl"></i>
+              </div>
+              {/* Description */}
+              <div className="border py-8">
+                <h1 className="text-center lg:text-5xl text-3xl text-gray-300">
+                  Run Battle
+                </h1>
+              </div>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
   );
