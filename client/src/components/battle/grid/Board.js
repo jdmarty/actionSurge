889e2fa5924729aecfae10b2ebcfe30d27617e8e
JD Marty <joshua.dmarty@gmail.com>
2021-02-27 13:51:35 -0800

    minor styling for grid

--- client/src/components/battle/grid/Board.js
+++ client/src/components/battle/grid/Board.js
@@ -21,7 +21,7 @@ function Board({ spl, combatants, mover, setMover, move }) {
       if (isTokenHere) {
         grid.push(
           <Square spl={spl} onMouseUp={() => move(x, y)} key={i}>
-            <Token {...isTokenHere} onMouseDown={setMover} mover={mover} />
+            <Token {...isTokenHere} onMouseDown={setMover} mover={mover}/>
           </Square>
         );
       } else {
@@ -39,7 +39,7 @@ function Board({ spl, combatants, mover, setMover, move }) {
   };
   // return a flexbox containing the prescribed number of squares
   return (
-    <div className="flex flex-wrap" style={{ height: "90%" }}>
+    <div className="flex flex-wrap p-1" style={{ height: "90%" }}>
       {renderBoard()}
     </div>
   );
